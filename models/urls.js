9a5ad4120af1a5fd953cbbc296e143d0627c5ed3
Ali Mousavi <ali.mousavi@gmail.com>
2016-12-24 16:39:32 +0330

    use shortid to get unique ids

--- models/urls.js
+++ models/urls.js
@@ -1,16 +1,21 @@
 /* jshint esversion: 6 */
 const mongoose = require('mongoose');
 const validator = require('validator');
+const shortid = require('shortid');
 
 const Schema = mongoose.Schema;
 
 const urlSchema = new Schema({
-    _id: { type: Number,
-           required: true,
-           unique: true },
-    redirect: { type: String,
-                required: true, 
-                validate: [validator.isURL , 'you should use a valid URL' ]  }
+    _id: { 
+        type: String,
+        default: shortid.generate()
+    },
+    redirect: { 
+        type: String,
+        required: true, 
+        validate: [ (v) => validator.isURL(v, {require_protocol: true}),
+                    'you should use a valid URL with a valid protocol' ] 
+    }
 });
 
 const Url = mongoose.model('Url', urlSchema);
--- test/urls.js
+++ test/urls.js
@@ -8,38 +8,22 @@ describe("Url", () => {
 
         url.validate((err) => {
             expect(err.errors.redirect).to.exist;
-            expect(err.errors._id).to.exist;
             done();
         })
     })
 
-    it("should be invalid for invalid _id", (done) => {
-        let url = new Url( {
-            _id: "Hey",
-            redirect: "http://www.alimsvi.com"
-        } );
-
-        url.validate((err) => {
-            expect(err.errors._id).to.exist;
-            expect(err.errors.redirect).to.not.exist;
-            done();
-        });
-    });
-
-    it("should be valid for alimsvi.com", (done) => {
+    it("should be invalid for alimsvi.com", (done) => {
         let url = new Url({
-            _id: 12,
             redirect: "alimsvi.ir"
         });
         url.validate((err) => {
-            expect(err).to.not.exist;
+            expect(err).to.exist;
             done();
         });
     });
 
     it("should be valid for https://alimsvi.com", (done) => {
         let url = new Url({
-            _id: 51,
             redirect: "https://alimsvi.ir"
         });
         url.validate((err) => {
