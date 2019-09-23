83f757bf38aee4b1414fda209ababac7587e7051
Ali Mousavi <ali.mousavi@gmail.com>
2019-09-23 21:46:22 +0330

    Update packages

--- routes/index.js
+++ routes/index.js
@@ -2,48 +2,50 @@
 var express = require('express');
 var router = express.Router();
 
-const Url = require("../models/urls");
+const Url = require('../models/urls');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tuxitop URL shortner API', baseURL: req.get('host') });
+  res.render('index', {
+    title: 'Tuxitop URL shortner API',
+    baseURL: req.get('host')
+  });
 });
 
 /* redirection */
 router.get('/:shortid', (req, res, next) => {
-    Url.findOne({_id: req.params.shortid}, (err, url) => {
-        if (err) {
-            return next(err);
-        }
-        if (!url) {
-            res.status(400).send({
-                error: "This URL is not found in the database"
-            });
-            return;
-        }
-        res.redirect(url.redirect);
-    });
+  Url.findOne({ _id: req.params.shortid }, (err, url) => {
+    if (err) {
+      return next(err);
+    }
+    if (!url) {
+      res.status(400).send({
+        error: 'This URL is not found in the database'
+      });
+      return;
+    }
+    res.redirect(url.redirect);
+  });
 });
 
 /* short url creation */
 router.get(/\/new\/(.+)/, (req, res, next) => {
-    let newUrl = new Url( {
-        redirect: req.params[0]
-    });
-    console.log(req.params[0]);
+  let newUrl = new Url({
+    redirect: req.params[0]
+  });
 
-    newUrl.save((err) => {
-        if (err) {
-            res.status(400).send({
-                error: err.errors.redirect ? err.errors.redirect.message : "Something happened!"
-            });
-            return;
-        }
-        res.send({
-            original_url: newUrl.redirect,
-            short_url: req.get('host') + "/" + newUrl._id
-        });
+  newUrl.save(err => {
+    if (err) {
+      res.status(400).send({
+        error: err.errmsg || 'Something happened!'
+      });
+      return;
+    }
+    res.send({
+      original_url: newUrl.redirect,
+      short_url: req.get('host') + '/' + newUrl._id
     });
+  });
 });
 
 module.exports = router;
